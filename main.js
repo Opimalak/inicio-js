838ac15493bd32e54a3dd1b26117e55a0832025d
Kaique Gabriel <149600790+Opimalak@users.noreply.github.com>
2024-04-11 16:41:12 -0300

    a

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
 const DataAtual = new Date();
 let DataObjetivo = prompt("Data Escolhida:");
 DataObjetivo = new Date(DataObjetivo+"T23:59:59");
+let diasQueFaltam = Math.floor((DataObjetivo-DataAtual)/86400000)
